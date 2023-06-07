546b796f41c708cc8f62b5af779355bcd69ace15
Dionne Spaltman <dionnespaltman@sp-byods-145-109-33-191.wireless.uva.nl>
2023-06-07 11:29:22 +0200

    Added super class

--- code/models.py
+++ code/models.py
@@ -5,28 +5,33 @@ class Board():
     """ Creates a board to which cars and trucks can be added """
 
     def __init__(self, width: int) -> None:
-        """ Creates a board object with a width. """
-        self.width=width
+        """ Creates a empty board object with a width (6x6, 9x9, 12x12). """
+        self.width = width
+
+class Vehicle(): 
+    """ Add a description """
+
+    def __init__(self, id: str, direction: Any, column: int, row: int) -> None:
+        self.id = id
+        self.direction = direction
+        self.column = column
+        self.row = row
 
 
-class Car():
+class Car(Vehicle):
     """ A car with a direction """
 
-    def __init__(self, direction: Any, column: int, row: int) -> None:
+    def __init__(self, id: str, direction: Any, column: int, row: int) -> None:
         """ Creates a car object with a size, a direction and position on the board"""
-        self.size=2
-        self.direction=direction
-        self.column=column
-        self.row=row
+        super().__init__(id, direction, column, row)
+        self.size = 2
 
 
-class Truck():
+class Truck(Vehicle):
     """ A truck with a direction"""
 
-    def __init__(self, direction: Any, column: int, row: int) -> None:
-        """ Creates a truck object with a direction and position on the board"""
-        self.size=3
-        self.direction=direction
-        self.column=column
-        self.row=row
+    def __init__(self, id: str, direction: Any, column: int, row: int) -> None:
+        """ Creates a truck object with a size, a direction and position on the board"""
+        super().__init__(id, direction, column, row)
+        self.size = 3
 
