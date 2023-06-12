de39498d865dbb11693fd50337cf98734c7d8b41
AmberElysa <9amber@live.nl>
2023-06-12 13:30:58 +0200

    time added in main.py

--- code/main.py
+++ code/main.py
@@ -1,9 +1,17 @@
 from algorithms.algorithm import Algorithm
 from classes.models import RushHour
+import time
 
 if __name__ == '__main__': 
+    
+    start_time = time.time()
+    
     game = RushHour(6, "gameboards/Rushhour6x6_1.csv")
     random_algorithm = Algorithm(game)
     random_algorithm.random_algorithm()
+    
+    end_time = time.time()
+    print(f"The random algorithm took {end_time - start_time} seconds.")
+    
 
     
