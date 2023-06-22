0030cfc68cb6e443b1020594ca3b6149e7db8fd8
Dionne Spaltman <dionnespaltman@MacBook-Pro-van-Dionne.local>
2023-06-22 17:10:12 +0200

    Tried implementing an archive for greedy

--- code/algorithms/beam_search.py
+++ code/algorithms/beam_search.py
@@ -6,10 +6,17 @@ from classes.vehicle import *
 
 class BeamSearch(BreadthFirst):
     """ 
-    This class gives implements a beam search algorithm
+    This class implements a beam search algorithm
+    It sorts the states based on three sorting methodes: 
+    - Distance from red car to exit 
+    - Number of vehicles that are blocking the red car 
+    - A combination of these two 
     """
-
-    def col_red_car(self) -> int: 
+    def col_red_car(self) -> int:
+        """ 
+        TO DO 
+        """
+ 
         # get location of red car 
         red_car = self.game.game_board.vehicle_dict["X"]
         
@@ -18,43 +25,43 @@ class BeamSearch(BreadthFirst):
 
         return col_red_car
     
-    def row_red_car(self) -> int: 
-        # get location of red car 
+    def row_red_car(self) -> int:
+        """ 
+        Get the row of the red car 
+        """
         red_car = self.game.game_board.vehicle_dict["X"]
-        
-        # only interested in the colummn 
         row_red_car: int = red_car.row
-
         return row_red_car
 
 
-    def amount_of_boxes(self, game:RushHour) -> int: 
+    def amount_of_boxes(self, game:RushHour) -> int:
+        """ 
+        Get the amount of boxes that are between the red car and the exit 
+        """
         # get width of board 
         width = self.game.game_board.width 
-
         # calculate boxes between red vehicle and exit 
         boxes = width - self.col_red_car()
-
         return boxes
+
 
     def sort_by_distance(self): 
         """ 
-        Sort the children states > self.stack 
-        Heuristic: states with red car closest to the exit are the best states 
+        Heuristic (h1): states with red car closest to the exit are the best states 
         Pre: a stack with children states
         Post: a sorted stack with children states 
         """
-
-        # this list needs to be sorted
         self.stack.sort(key=self.amount_of_boxes)
 
     def count_blocking_vehicles(self, game:RushHour) -> int: 
+        """ 
+        Heuristic (h2): states with no or very little blocking vehicles are the best states
+        """
         # get row and column of red car 
         row_red_car: int = self.row_red_car()
         col_red_car: int = self.col_red_car()
         width = self.game.game_board.width 
 
-        # this is the amount of vehicles that are blocking the red car 
         count = 0 
 
         # loop through the boxes in front of the red car 
@@ -69,18 +76,33 @@ class BeamSearch(BreadthFirst):
 
 
     def sort_by_blocking_vehicles(self):
+        """ 
+        Using h1 to sort
+        """
         self.stack.sort(key=self.count_blocking_vehicles)
+    
+    def combination_heuristic(self, game:RushHour) -> int: 
+        """ 
+        This combines the distance from the target vehicle and the numnber of vehicles
+        that block the way to the exit
+        
+        The lower, the better
+        """
+        count = self.game.count_blocking_vehicles()
+        amount_of_boxes = self.game.amount_of_boxes()
 
+        combination = count * amount_of_boxes
 
-    def build_children(self, beamsize: int = 10) -> None: 
+        return combination 
+
+    def build_children(self, beamsize: int = 10, sorting_method: str = 'h1') -> None: 
         """""
         Creates all possible child-states and adds them to the list of states.
-        lengte van stack moet altijd hoogstens zo groot zijn als beam size
+        Length of stack needs to be at least as high as the beam size 
         """""
         moves = [] 
-
+        
         # Loop over all the different states the vehicles can be in
-        # End: list of all states
         for vehicle_id in self.game.get_vehicle_ids():
             for direction in [-1, 1]:
                 if self.game.game_board.is_move_valid(vehicle_id, direction):
@@ -99,11 +121,20 @@ class BeamSearch(BreadthFirst):
                 self.stack.append(copy.deepcopy(self.game))
             self.game.process_undo()
 
-        # sort the stack so that the most promising are at the front 
-        self.sort_by_distance()
+        if sorting_method == "h1": 
+            # sort the stack so that the most promising are at the front (from short to long distance)
+            print("beam search: sort by distance")
+            self.sort_by_distance()
 
-        # sort the stack so that the states with the least blocking cars in front of red car 
-        # self.sort_by_blocking_vehicles()
+        elif sorting_method == "h2": 
+            # sort the stack so that the states with the least blocking cars in front of red car
+            print("beam search: sort by blocking vehicles")
+            self.sort_by_blocking_vehicles()
+
+        else: 
+            # sort the stack with a combination of the two heuristics (from low to high)
+            print("beam search: combination of the two heuristics")
+            self.combination_heuristic()
 
         # if len(self.stack) > beamsize:
         #     print(len(self.stack))
--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -11,27 +11,31 @@ class Greedy(Algorithm):
     If that's not the case, it checks whether it can move the car that blocking the red car. 
     If that's not the case, it selects a random move. 
     """
-    def __init__(self, game: RushHour) -> None: 
+    def __init__(self, game:RushHour) -> None: 
         self.game = game
         self.vehicles = game.get_vehicles()
         self.vehicle_ids = game.get_vehicle_ids()
         self.directions = [1, -1]
-        self.visited_states = set()
 
-        # make this a global variable that is easily accessible in all the methods
-        self.last_move: tuple[str, int] = 'A', 1
+        self.archive = set()
+
+        # self.last_move: tuple[str, int] = 'A', 1
     
     def move_red_car(self) -> bool: 
         """
         Moves the red car if that's possible 
         If it moves, self.last_move is set to the new move 
         """
-        red_move = self.game.process_turn("X", 1)
-        if red_move:
+        self.game.process_turn("X", 1)
+        visited = self.game.get_board_hash()
+
+        if visited not in self.archive:
+            self.archive.add(visited)
             # print("red move")
-            # set last move 
-            self.last_move = 'X', 1
+            # self.last_move = 'X', 1
             return True 
+        
+        self.game.process_undo()
         return False 
     
     def move_blocking_vehicle(self) -> bool: 
@@ -47,49 +51,47 @@ class Greedy(Algorithm):
         if blocking_vehicle: 
             # choose a random direction for vehicle 
             direction = self.choose_direction()
+            self.game.process_turn(blocking_vehicle.id, direction)
+            visited = self.game.get_board_hash() 
 
-            # if we are undoing the last move, return false
-            if self.check_last_move(blocking_vehicle.id, direction):
-                return False 
-            
-            # move blocking vehicle 
-            else: 
-                self.game.process_turn(blocking_vehicle.id, direction)
-                # set last move 
-                self.last_move = blocking_vehicle.id, direction
-                return True
+            if visited not in self.archive:
+                self.archive.add(visited)
+                return True 
         
-        # if no blocking vehicle was found or could be moved 
-        return False
+            self.game.process_undo()
+            return False 
+
     
     def move_random_car(self) -> bool: 
         # get a list of vehicles that can be moved  including the direction
         movable_vehicles = self.game.get_movable_vehicles()
+        
         # choose a random vehicle and direction 
         vehicle, direction = self.choose_vehicle_from_movable_vehicles(movable_vehicles)
 
-        # check if we are not undoing the last move 
-        if self.check_last_move(vehicle, direction): 
-            if self.game.process_turn(vehicle, direction): 
-                # set last move 
-                self.last_move = vehicle, direction
-                return True
-        else: 
-            return False 
-    
-    def check_last_move(self, vehicle: str, direction: int) -> bool:
-        if self.last_move[0] == vehicle and self.last_move[1] == direction * -1: 
-            return False
-        else:
+        self.game.process_turn(vehicle, direction)
+        visited = self.game.get_board_hash() 
+
+        if visited not in self.archive:
+            self.archive.add(visited)
             return True 
+    
+        self.game.process_undo()
+        return False 
+    
+    # def check_last_move(self, vehicle: str, direction: int) -> bool:
+    #     if self.last_move[0] == vehicle and self.last_move[1] == direction * -1: 
+    #         return False
+    #     else:
+    #         return True 
 
     def run(self) -> int:
         counter = 0
 
         while not self.game.is_won():
         # while counter <= 40:
-            # print("")
-            # self.game.show_board()
+            print("")
+            self.game.show_board()
             counter += 1
 
             # try to move red car to the right
