2f15b685bce635b34eee347048acdb563e734e16
pluumenbrownie <git.wessel@beumertjes.nl>
2023-06-23 15:35:34 +0200

    some misc. stuff

--- code/algorithms/GraphBased.py
+++ code/algorithms/GraphBased.py
@@ -12,15 +12,16 @@ class Dijkstra:
         self.board_file = board_file
         self.graph = Graph(board_size, board_file)
     
-    def build_graph(self) -> None:
+    def build_graph(self, max_iterations: int = 10_000_000, max_useless: int = 10_000) -> None:
         statespace = count_statespace(self.board_size, self.board_file)
         print(f"{statespace=}")
-        print(f"Equals {statespace/10_000_000:.2f}x max_iterations")
-        self.graph.build_graph(100_000_000)
+        print(f"Equals {statespace/max_iterations:.2f}x max_iterations")
+        self.graph.build_graph(max_iterations, max_useless)
         self.graph.stats()
     
     def run(self) -> None:
         print("Started Dijkstra solver")
+        nodes_considered = 0
         self.processing_queue = SortedList()
         starting_hash = self.graph.starting_node
         # visited_states: set[Node] = set()
@@ -29,8 +30,9 @@ class Dijkstra:
         while not self.current_node.is_won:
             self.extend_queue()
             self.current_node = self.processing_queue.pop(0)
+            nodes_considered += 1
         
-        print("Reached winning node.")
+        print(f"Reached winning node. Concidered {nodes_considered} nodes.")
     
     def extend_queue(self) -> None:
         for connecting_node in self.current_node.connections:
@@ -52,3 +54,4 @@ class Dijkstra:
             file.write("car,move\n")
             for id, direction in moves:
                 file.write(f"{id},{direction}\n")
+        print(f"Exported solution of {len(moves)} moves.")
