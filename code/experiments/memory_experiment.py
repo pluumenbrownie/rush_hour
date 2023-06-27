8b810f77fbc0ee51ef1911cdb5addae5309f9cc8
pluumenbrownie <git.wessel@beumertjes.nl>
2023-06-27 23:44:08 +0200

    Finished experiment README

--- code/experiments/memory_experiment.py
+++ code/experiments/memory_experiment.py
@@ -8,24 +8,24 @@ from copy import deepcopy
 def memory_comparison(board_size: int, board_file: str) -> None:
     tracemalloc.start()
     game = RushHour(board_size, board_file)
-    breadthfirst_algorithm = BreadthFirst(game)
-    breadth_amount = breadthfirst_algorithm.run()
+    algorithm = BreadthFirst(game)
+    breadth_amount = algorithm.run()
     _, mem_peak_breadth = tracemalloc.get_traced_memory()
     tracemalloc.reset_peak()
 
     game = RushHour(board_size, board_file)
-    branch_algorithm = BranchAndBound(game)
-    branch_amount = branch_algorithm.run(first_only=False)
+    algorithm = BranchAndBound(game, bound=100)
+    branch_amount = algorithm.run(first_only=False)
     _, mem_peak_branch = tracemalloc.get_traced_memory()
     tracemalloc.reset_peak()
 
     beams = 50
     game = RushHour(board_size, board_file)
-    beam_algorithm = BeamSearch(game)
-    beam_amount = beam_algorithm.run('h2', beams)
+    algorithm = BeamSearch(game)
+    beam_amount = algorithm.run('h2', beams)
     _, mem_peak_beam = tracemalloc.get_traced_memory()
     tracemalloc.reset_peak()
 
     print(f"Breadth first:    {mem_peak_breadth:12} Bytes, {breadth_amount:3} moves.")
     print(f"Branch and Bound: {mem_peak_branch:12} Bytes, {branch_amount:3} moves.")
-    print(f"Beam first:       {mem_peak_beam:12} Bytes, {beam_amount:3} moves.")
+    print(f"Beamsearch({beams}):   {mem_peak_beam:12} Bytes, {beam_amount:3} moves.")
